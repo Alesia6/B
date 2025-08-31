ebf4f3855ed20aa2321fdfbdd013ba7411c12c3c
Alesia6 <alesiaalikaj6@gmail.com>
2025-08-31 19:28:16 +0200

    login in if valid user

--- src/server.js
+++ src/server.js
@@ -23,4 +23,38 @@ app.use(
 
 app.use(express.static(path.join(__dirname, '../public')));
 
+function requireLogin(req, res, next) {
+    if (!req.session.accountId) {
+        return res.status(401).json({error: 'NOT logged in'});
+    }
+    next();
+}
+
+app.post('/login', async (req, res) => {
+    try {
+        const  { cardNumber, pin } = req.body;
+        if (!cardNumber || pin) {
+            return res.status(400).json({error: 'cardNumber and pin are required'});
+        }
+
+        const [rows] = await pool.query(
+            'SELECT id, pin_hash FROM accounts WHERE card_number = ? LIMIT 1',
+            [cardNumber]
+        );
+        if (rows.length === 0) {
+            return res.status(400).json({ error: 'Invalid card or pin' });
+        }
+
+        const ok = await bcrypt.compare(pin, rows[0].pin_hash);
+        if (!ok) {
+            return res.status(400).json({ error: 'Invalid card or pin' });
+        }
+
+        req.session.accountId = rows[0].id;
+        res.json({ message: 'Logged in'});
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
 
